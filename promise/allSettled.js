83abe9f3082c6659de30a769a0b8c8b1fe1fff83
yangtianbo5 <yangtianbo5@hikvision.com.cn>
2020-09-23 17:01:40 +0800

    1

--- promise/allSettled.js
+++ promise/allSettled.js
@@ -3,16 +3,17 @@
  * @Date: 2020-09-23 16:48:36
  * @Description: 
  * @LastEditors: yangtianbo5
- * @LastEditTime: 2020-09-23 16:54:59
+ * @LastEditTime: 2020-09-23 17:01:02
  */
 const fetch = function (idx) {
-  return new Promise((resolve) => {
+  return new Promise((resolve,reject) => {
     console.log(`start request ${idx}`)
     // 模拟请求时间
     const timeout = parseInt(Math.random() * 1e4)
     setTimeout(() => {
       console.log(`end request ${idx}`)
-      resolve(idx)
+    
+      Math.random()>=0.5?resolve(idx):reject(idx)
     }, timeout)
   })
 }
@@ -25,10 +26,15 @@ promiseAllSetteld=function (arr) {
     let result=[]
     let handle=function(data,i,flag) {
       count++
-      result[i]={
+      let tar={
         status:flag?'fullfilled':'reject',
-        value:data
       }
+      if(flag){
+        tar.value=data
+      }else{
+        tar.reason=data
+      }
+      result[i]=tar
       if(count>=len){
         resolve(result)
       }
